7bd8f9932bcada2afa7549515799d747c68cfd83
Shama <assiddik001@gmail.com>
2022-03-06 22:57:20 +0600

    top header done

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+const showSearch = () => {
+    document.getElementById('searchBox').style.display = "inline-block";
+    document.getElementById('top-search-ico-close').style.display = "inline-block";
+}
+
+const closeSearch = () => {
+    document.getElementById('searchBox').style.display = "none";
+    document.getElementById('top-search-ico-close').style.display = "none";
+}
+
