0da49f3682b2a95445fe2b6489998d00167b0e5a
Shama <assiddik001@gmail.com>
2022-03-08 00:34:24 +0600

    menu fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,37 +8,50 @@ const closeSearch = () => {
     document.getElementById('top-search-ico-close').style.display = "none";
 }
 
-(function($){
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+(function ($) {
     "use strict";
-	jQuery(document).ready(function($){
+    jQuery(document).ready(function ($) {
         $(".products-carousel").owlCarousel({
-                margin: 30,
-                loop: true,
-                dots: true,
-                nav: false,
-                autoplay: false,
-                responsive : {
-                    // breakpoint from 0 up
-                    0 : {
-                        items: 1
-                    },
-                    // breakpoint from 768 up
-                    768 : {
-                        items: 2
-                    },
-                    // breakpoint from 992 up
-                    992 : {
-                        items: 3
-                    }
+            margin: 30,
+            loop: true,
+            dots: true,
+            nav: false,
+            autoplay: false,
+            responsive: {
+                // breakpoint from 0 up
+                0: {
+                    items: 1
+                },
+                // breakpoint from 768 up
+                768: {
+                    items: 2
+                },
+                // breakpoint from 992 up
+                992: {
+                    items: 3
                 }
+            }
         });
     });
 
-    jQuery(window).load(function(){
-     
-        
+    jQuery(window).load(function () {
+
+
     });
-    
+
 }(jQuery));
 
 
