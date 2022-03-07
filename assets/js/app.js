13d7005a2e2f6be63a4b9ec48de3a61fbb6bf8ec
Shama <assiddik001@gmail.com>
2022-03-08 01:49:13 +0600

    final check

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,38 +20,3 @@ function myFunction() {
         header.classList.remove("sticky");
     }
 }
-
-(function ($) {
-    "use strict";
-    jQuery(document).ready(function ($) {
-        $(".products-carousel").owlCarousel({
-            margin: 30,
-            loop: true,
-            dots: true,
-            nav: false,
-            autoplay: false,
-            responsive: {
-                // breakpoint from 0 up
-                0: {
-                    items: 1
-                },
-                // breakpoint from 768 up
-                768: {
-                    items: 2
-                },
-                // breakpoint from 992 up
-                992: {
-                    items: 3
-                }
-            }
-        });
-    });
-
-    jQuery(window).load(function () {
-
-
-    });
-
-}(jQuery));
-
-
